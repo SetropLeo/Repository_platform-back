0626b3b18bebddf979d43b924ac9f80fe151833a
LeonardoPortes <leol_portes@hotmail.com>
2022-07-04 19:27:52 -0300

    Create basic Auth middleware and implement it in the routes file

---
+++ src/middlewares/auth.js
@@ -0,0 +1,11 @@
+export default async (req, res, next) => {
+  const authenticated = false;
+
+  if (authenticated) {
+    return next();
+  } else {
+    return res.status(401).json();
+  }
+
+  return next();
+};

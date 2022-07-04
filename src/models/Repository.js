4ddc8ad9ad22d7ad27c0456f960b1003ce82c076
LeonardoPortes <leol_portes@hotmail.com>
2022-07-04 18:24:54 -0300

    Create models for Repository and User

---
+++ src/models/Repository.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);

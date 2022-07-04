1b60bbb7c0a2718308817ea376796013978adcc1
LeonardoPortes <leol_portes@hotmail.com>
2022-07-04 19:50:51 -0300

    Create function to compare normal password with encrypted password

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,3 +3,7 @@ import bcrypt from 'bcryptjs';
 export const createPasswordHash = async (password) => {
   return bcrypt.hash(password, 8);
 };
+
+export const checkPassword = (user, password) => {
+  return bcrypt.compare(password, user.password);
+};

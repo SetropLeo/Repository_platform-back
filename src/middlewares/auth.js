2be12c2aab4ad426c52819b849322db88bb8c9ea
LeonardoPortes <leol_portes@hotmail.com>
2022-07-04 20:15:29 -0300

    Create Auth middleware to verify bearer token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,13 +1,23 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
+import authConfig from '../config/auth';
+import { promisify } from 'util';
 
 export default async (req, res, next) => {
-  const authenticated = false;
+  const authHeader = req.headers.authorization;
 
-  if (authenticated) {
-    return next();
-  } else {
-    return res.status(401).json();
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token was not provided.' });
   }
 
-  return next();
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token.' });
+  }
 };
